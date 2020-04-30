5fdac5fd97656ccd0a07b841c9d48e9b5ba63edb
Aldwyn Cabarrubias <aldwyn.f.cabarrubias@accenture.com>
2020-05-01 01:26:34 +0800

    Update compact layout text

--- src/layouts/compact.ts
+++ src/layouts/compact.ts
@@ -10,6 +10,9 @@ export function formatCompactLayout(
   const runLink = `${repoUrl}/actions/runs/${process.env.GITHUB_RUN_ID}`;
   const webhookBody = new WebhookBody();
   const status = "STARTED";
-  webhookBody.text = `\`${status}\` CI [#${process.env.GITHUB_RUN_NUMBER}](${runLink}) (commit [${shortSha}](${commit.data.html_url})) on [${process.env.GITHUB_REPOSITORY}](${repoUrl}) by [(@${author.login})](${author.html_url})`;
+  webhookBody.text =
+    `\`${status}\` &nbsp; CI [#${process.env.GITHUB_RUN_NUMBER}](${runLink}) ` +
+    `(commit [${shortSha}](${commit.data.html_url})) on [${process.env.GITHUB_REPOSITORY}](${repoUrl}) ` +
+    `by [@${author.login}](${author.html_url})`;
   return webhookBody;
 }
