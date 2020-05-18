d7ec1481ce5f10b1a9c41164a8195fce99842c2f
Aldwyn Cabarrubias <aldwyn.f.cabarrubias@accenture.com>
2020-05-18 21:33:12 +0800

    Check process.env variables

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import { setFailed, saveState, getState } from "@actions/core";
+import { setFailed } from "@actions/core";
 import { formatAndNotify } from "./utils";
 
 try {
   formatAndNotify("start");
-  saveState("startTime", new Date().toUTCString());
-  console.log(process.env);
-  console.log(new Date().toUTCString(), getState("startTime"));
 } catch (error) {
   setFailed(error.message);
 }
--- src/post.ts
+++ src/post.ts
@@ -1,8 +1,18 @@
-import { setFailed } from "@actions/core";
-import { formatAndNotify } from "./utils";
+import { setFailed, getInput } from "@actions/core";
+import { formatAndNotify, getRunInformation } from "./utils";
+import { Octokit } from "@octokit/rest";
 
 try {
+  // const runInfo = getRunInformation();
+  // const githubToken = getInput("github-token", { required: true });
+  // const octokit = new Octokit({ auth: `token ${githubToken}` });
+  // const workflowJob = await octokit.actions.getWorkflowJob({
+  //   owner: runInfo.owner,
+  //   repo: runInfo.repo,
+  //   job_id: runInfo.,
+  // });
   formatAndNotify("exit");
+  console.log(process.env);
 } catch (error) {
   setFailed(error.message);
 }
