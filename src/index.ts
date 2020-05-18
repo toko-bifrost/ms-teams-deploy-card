6f6aa0618a377e1b79cb5cc8be8c3dc39b59948e
Aldwyn Cabarrubias <aldwyn.f.cabarrubias@accenture.com>
2020-05-18 19:39:58 +0800

    Further evaluate startTime state at index.js

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import { setFailed, saveState } from "@actions/core";
+import { setFailed, saveState, getState } from "@actions/core";
 import { formatAndNotify } from "./utils";
 
 try {
   formatAndNotify("start");
   saveState("startTime", new Date());
+  console.log(getState("startTime"));
 } catch (error) {
   setFailed(error.message);
 }
